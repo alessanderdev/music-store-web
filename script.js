6d26d75c1b6fc61f7f95607b3526a3504a37747d
Alexander Luyo <alexander.valusk@gmail.com>
2025-06-06 10:10:36 -0500

    alexander: creacion base de pagina web

---
+++ script.js
@@ -0,0 +1,9 @@
+function buscar() {
+    const query = document.getElementById('searchInput').value;
+    alert('Buscando: ' + query);
+}
+
+function verTodo() {
+    alert("Redirigiendo a la colección completa de Fender...");
+    // window.location.href = 'coleccion-fender.html'; // Descomenta si tienes esa página
+}
