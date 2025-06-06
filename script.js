68e58cec6dd5b6df3fea842be5cf436dfb9e7c93
Alexander Luyo <alexander.valusk@gmail.com>
2025-06-06 13:50:25 -0500

    Agregar base 01

---
+++ script.js
@@ -0,0 +1,8 @@
+function buscar() {
+    const query = document.getElementById('searchInput').value;
+    alert('Buscando: ' + query);
+}
+
+function verTodo() {
+    alert("Redirigiendo a la colección completa de Fender...");
+}
